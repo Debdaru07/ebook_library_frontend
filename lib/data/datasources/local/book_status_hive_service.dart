0c90638240a9bbef143023fec445dd871eb8faec
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2026-01-19 22:24:59 +0530

    hive init lifecycle issue resolved

--- lib/data/datasources/local/book_status_hive_service.dart
+++ lib/data/datasources/local/book_status_hive_service.dart
@@ -3,30 +3,43 @@ import '../../models/hive_book_status_model.dart';
 import '../../../core/constants/hive_constants.dart';
 
 class BookStatusHiveService {
-  late Box<HiveBookStatusModel> _box;
+  Box<HiveBookStatusModel>? _box;
 
   Future<void> init() async {
-    _box = Hive.box<HiveBookStatusModel>(HiveConstants.bookStatusBox);
+    if (!Hive.isBoxOpen(HiveConstants.bookStatusBox)) {
+      _box = await Hive.openBox<HiveBookStatusModel>(
+        HiveConstants.bookStatusBox,
+      );
+    } else {
+      _box = Hive.box<HiveBookStatusModel>(HiveConstants.bookStatusBox);
+    }
+  }
+
+  Box<HiveBookStatusModel> get box {
+    if (_box == null) {
+      throw Exception('BookStatusHiveService not initialized');
+    }
+    return _box!;
   }
 
   bool isBookRead(String bookId) {
-    final status = _box.get(bookId);
+    final status = box.get(bookId);
     return status?.isRead ?? false;
   }
 
   Future<void> toggleBookStatus(String bookId) async {
-    final existing = _box.get(bookId);
+    final existing = box.get(bookId);
 
     if (existing != null) {
       existing.isRead = !existing.isRead;
       await existing.save();
     } else {
-      await _box.put(bookId, HiveBookStatusModel(bookId: bookId, isRead: true));
+      await box.put(bookId, HiveBookStatusModel(bookId: bookId, isRead: true));
     }
   }
 
   Map<String, bool> getAllStatuses() {
-    return _box.values.fold({}, (map, item) {
+    return box.values.fold({}, (map, item) {
       map[item.bookId] = item.isRead;
       return map;
     });
